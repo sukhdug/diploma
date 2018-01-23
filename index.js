72bb3929e289667ba5f828ceaad8ac1e58a2c0d7
Handy-Surun Dugurzhap <handy-surun.dugurzhap@office.e2e4gu.ru>
2018-01-23 18:27:34 +0700

    index.js changed: bot reply to messages

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 // Токен телеграм-бота
-var token = '488077289:AAHu1Tv8ITPDnicBMkjQDiczFHFDLlfoa30';
+const token = '488077289:AAHu1Tv8ITPDnicBMkjQDiczFHFDLlfoa30';
 
 // Включить опрос сервера
-var bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/help (.+)/, (msg, match) =>  {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    var hi = 'hi';
+    if (msg.text.toString().toLowerCase().indexOf('hi') === 0) {
+      bot.sendMessage(chatId, "Hello");
+    }else if (msg.text.toString().indexOf('How are you?') === 0) {
+      bot.sendMessage(chatId, "I'm fine, and you?");
+    }
+});
