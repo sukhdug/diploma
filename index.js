4ba3944f6abcd7aa863d3fe30e1568ceb6b3c82d
Handy-Surun Dugurzhap <handy-surun.dugurzhap@office.e2e4gu.ru>
2018-01-24 17:29:46 +0700

    Update Node modules and index.js

--- index.js
+++ index.js
@@ -16,10 +16,13 @@ bot.onText(/\/help (.+)/, (msg, match) =>  {
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
+    const user = msg.chat.username;
     var hi = 'hi';
     if (msg.text.toString().toLowerCase().indexOf('hi') === 0) {
-      bot.sendMessage(chatId, "Hello");
-    }else if (msg.text.toString().indexOf('How are you?') === 0) {
+      bot.sendMessage(chatId, "Hello " + user + "!");
+    }else if (msg.text.toString().indexOf('how are you?') === 0) {
       bot.sendMessage(chatId, "I'm fine, and you?");
+    }else{
+      bot.sendMessage(chatId, "I don't understand you, " + user + "! Sorry");
     }
 });
